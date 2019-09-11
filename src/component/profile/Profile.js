dcd130961d38c2384c9f89e3e8d6b7fe5cdf051e
JHall4475 <jhall4475@yaoo.com>
2019-09-11 01:35:17 -0600

    changes

--- src/component/profile/Profile.js
+++ src/component/profile/Profile.js
@@ -1,17 +1,34 @@
 import React, {Component} from 'react';
 import './profile.css';
+import axios from 'axios';
+import{connect} from 'react-redux';
 
 
 class Profile extends Component {
-    state={}
+    state={
+        username: this.props.userProfile.username,
+        profilePic: this.props.userProfile.profile_pic
+    }
+
+    onInputChangeUsername= (e) => {
+        this.setState({username: e.target.value})
+    }
+    OnChangeProfilePic = (e) => {
+        this.setState({profilePic: e.target.value})
+    }
 
     render(){
         return(
             <div className="profile-wrapper">
                 <div className="profile-container">
-                    <h3>User Profile</h3>
+                    <div>User Profile</div>
+                        <img className="profile-img" src={this.props.userProfile.profile_pic} alt="profile chosen by user"></img>
                     <div>
-                    Username: <input></input>
+                    Username: 
+                    <input
+                    onChange={this.onInputChangeUsername}
+                    value={this.state.username}
+                    ></input>
                     </div>
                     <div>
                         Password: <input></input>
@@ -20,7 +37,11 @@ class Profile extends Component {
                         Personal Goal Quote: <input></input>
                     </div>
                     <div>
-                        Profile Picture: <input></input>
+                        Profile Picture: 
+                        <input
+                        onChange={this.OnChangeProfilePic}
+                        value={this.state.profilePic}
+                        ></input>
                     </div>
                     
                     
@@ -31,4 +52,10 @@ class Profile extends Component {
     }
 }
 
-export default Profile;
+const mapStateToProps = (state) => {
+   return{ 
+       userProfile: state.userProfile,
+   }
+}
+
+export default connect(mapStateToProps)(Profile);
