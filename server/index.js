e675e434b5fa5d584441be6bb73c0d310519357d
JHall4475 <jhall4475@yaoo.com>
2019-10-10 20:50:33 -0600

    changes to host

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const controller = require('../server/controller');
-const massive = require('massive');
-const cors = require('cors');
-const session = require('express-session');
-const port = 8080;
+const path = require('path')
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var controller = require('../server/controller');
+var massive = require('massive');
+var cors = require('cors');
+var session = require('express-session');
+var port = 8080;
 
 app.use(cors());
+app.use(express.static(`${__dirname}/../build`));
 app.use(bodyParser.json());
 app.use(session({
     resave: true,
@@ -18,15 +20,15 @@ app.use(session({
 
 
 
-massive (process.env.DATABASE_STRING)
-.then(db => {
-    app.set('db', db)
-    console.log('Database is Connected Boss')
-    
-})
-.catch(err => {
-    console.log('Database connection error', err)
-})
+massive(process.env.DATABASE_STRING)
+    .then(db => {
+        app.set('db', db)
+        console.log('Database is Connected Boss')
+
+    })
+    .catch(err => {
+        console.log('Database connection error', err)
+    })
 
 let requireAuth = (req, res, next) => {
     if (!req.session.user) {
@@ -36,9 +38,9 @@ let requireAuth = (req, res, next) => {
     }
 }
 
-app.get('/health', (req, res) => {return res.send('ok')})
+app.get('/health', (req, res) => { return res.send('ok') })
 app.get('/api/workout/retrieve/:id', controller.retrieveWorkout)
-app.get('/api/user', (req, res) => {res.send(req.session.user)})
+app.get('/api/user', (req, res) => { res.send(req.session.user) })
 app.get('/api/weightretrieve/:id', controller.retrieveWeight)
 app.get('/api/chart/labels', controller.getLabels)
 
@@ -55,6 +57,10 @@ app.put('/api/workout/post', controller.addToWorkout)
 
 app.delete('/api/workout/:id', controller.deleteWorkoutItem)
 app.delete('/api/weight/:id', controller.deleteWeightEntry)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+})
+
 
 app.listen(port, () => console.log(`listening on ${port}`)
 )
